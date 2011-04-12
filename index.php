ab8ed3e678b2ea86c9f30bf09867c3ee0a6540ff
Jesse van den Kieboom <jesse.vandenkieboom@epfl.ch>
2011-04-12 17:13:43 +0200

    Added user filter

--- index.php
+++ index.php
@@ -17,8 +17,11 @@ class Home extends Controller
 
 	function action_index()
 	{
-		$this->jobs = Command::jobs();
+		$this->all_jobs = Command::jobs();
+		$this->jobs = array();
 		$form = new Form($this, 'get');
+		$this->users = array();
+		$this->user_selected = $form->user_filter;
 
 		$toggled = explode(',', $form->toggled);
 		$toggleitems = array();
@@ -30,13 +33,27 @@ class Home extends Controller
 			$toggleitems[intval($parts[0])] = array_slice($parts, 1);
 		}
 
-		foreach ($this->jobs as $job)
+		foreach ($this->all_jobs as $job)
 		{
+			if (!in_array($job->user, $this->users))
+			{
+				$this->users[] = $job->user;
+			}
+
+			if ($form->user_filter && $form->user_filter != $job->user)
+			{
+				continue;
+			}
+
 			if (array_key_exists($job->id, $toggleitems))
 			{
 				$job->toggled = $toggleitems[$job->id];
 			}
+
+			$this->jobs[] = $job;
 		}
+
+		sort(&$this->users);
 
 		$this->view();
 	}
